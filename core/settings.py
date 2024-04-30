c43ee793092f03e615f8bcd413cbea7a6dc741fb
Clement Idemudo <Clemcy9@gmail.com>
2024-04-30 21:47:46 +0100

    81% done

--- core/settings.py
+++ core/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['templates', '/home/melcat/funeral/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
